dd2a96d2618e2c2920ae0999990bbab8e1033fa1
Ayush Singh <ayushsingh1325@gmail.com>
2020-04-24 13:05:24 +0530

    feat: Can identify video files.

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"sync"
+)
+
+var videoFormats = []string{"mp4", "mkv"}
+
+func readFileInfo(fileName, filePath string, wg *sync.WaitGroup)  {
+	defer wg.Done()
+	for _, ext := range videoFormats {
+		stat, _ := filepath.Match("*." + ext, fileName)
+		if stat {
+			fmt.Println(fileName)
+		}
+	}
+}
+
+func recursiveRead(dirPath string, wg *sync.WaitGroup)  {
+	defer wg.Done()
+	items, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		fmt.Println("Error Reading Dir: ", err)
+		return
+	}
+	for _, item := range items {
+		wg.Add(1)
+		path := filepath.Join(dirPath, item.Name())
+		if item.IsDir() {
+			go recursiveRead(path, wg)
+		} else {
+			go readFileInfo(item.Name(), path, wg)
+		}
+	}
+}
+
+func scan(dirPath string)  {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go recursiveRead(dirPath, &wg)
+	wg.Wait()
+}
 
 func main()  {
-	fmt.Println("Hello World")
+	testPath, err := filepath.Abs("test")
+	if err != nil {
+		fmt.Println("Error getting current path", err)
+	}
+	scan(testPath)
 }
